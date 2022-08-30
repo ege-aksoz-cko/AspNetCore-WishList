c7f8c1d9ba36bdd64d8191197c02b2b50ec6bd11
egeaksoz-cko <ege.aksoz@checkout.com>
2022-08-30 16:12:48 +0100

    Les 6 fix 1

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -23,7 +23,8 @@ namespace WishList.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View("Index");
+            var model = _context.Items.ToList();
+            return View("Index", model);
         }
 
         [HttpGet]
@@ -40,9 +41,9 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            Item item = new Item() { Id = Id };
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
